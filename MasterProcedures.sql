bbef296f7f30852d054b145e12dbc40e8fb6f77f
gbordze <gbordze@miamioh.edu>
2017-05-03 10:53:09 -0400

    Made changes to error messages in MasterProcedures.sql

--- MasterProcedures.sql
+++ MasterProcedures.sql
@@ -7,7 +7,7 @@ AS
 	
 	IF NOT EXISTS(SELECT NULL FROM Users WHERE email = @email)
 		BEGIN
-			SELECT [errorMessage] = 'Sorry we could not find your email address'
+			SELECT [error] = 'Sorry we could not find your email address'
 		END 
 	ELSE IF EXISTS(SELECT NULL FROM Users WHERE (email = @email) AND [password] = PWDENCRYPT(@password) AND deleted = 0)
 		BEGIN
@@ -15,7 +15,7 @@ AS
 		END 
 	ELSE 
 		BEGIN
-			SELECT [errorMessage] = 'Could not find your password or email address'
+			SELECT [error] = 'Could not find your password or email address'
 		END
 
 
@@ -43,7 +43,6 @@ GO
 CREATE PROCEDURE updateUser
 	@userID int,
 	@email varchar(255),
-	@password varchar(255),
 	@name varchar(255),
 	@birthday date,
 	@weight float,
@@ -54,13 +53,12 @@ AS
 
 	IF NOT EXISTS(SELECT NULL FROM Users WHERE userID = @userID)
 		BEGIN
-			SELECT [errorMessage] = 'Could not find user to update'
+			SELECT [error] = 'Could not find user to update'
 		END 
 	ELSE
 		BEGIN
 			UPDATE Users
 			SET email = ISNULL(@email, email),
-				[password] = ISNULL(PWDENCRYPT(@password), [password]),
 				[name] = ISNULL(@name, [name]),
 				birthday = ISNULL(@birthday, birthday),
 				[weight] = ISNULL(@weight, [weight]),
@@ -69,6 +67,24 @@ AS
 			WHERE userID = @userID
 			SELECT userID, email, [name], birthday, [weight], [height] FROM Users WHERE userID = userID
 		END
+
+GO
+CREATE PROCEDURE updateUserPassword
+	@userID int,
+	@oldPassword varchar(255),
+	@newPassword varchar(255)
+AS
+	SET NOCOUNT ON 
+	IF EXISTS(SELECT NULL FROM Users WHERE (userID = @userID) AND
+											   ([password] = PWDENCRYPT(@oldPassword)))
+	BEGIN
+		UPDATE Users
+		SET [password] = PWDENCRYPT(@newPassword)
+		WHERE userID = @userID
+
+	END ELSE BEGIN
+ 		SELECT [error] = 'Password could not be updated'
+	END
 
 
 GO
@@ -82,10 +98,10 @@ AS
 		UPDATE Users
 		SET deleted = 1
 		WHERE userID = @userID
-		SELECT [successfulDelete] = 'User was successfully deleted.'
+		SELECT userID, [name], email, birthday, [height], [weight], trainer FROM Users WHERE userID = @userID 
 	END 
 	ELSE BEGIN
- 		SELECT [errorMessage] = 'User must be a trainer, so could not be deleted.'
+ 		SELECT [error] = 'User must be a trainer, so could not be deleted.'
 	END
 GO
 CREATE PROCEDURE addWorkout
@@ -98,14 +114,14 @@ AS
 	
 	IF NOT EXISTS(SELECT NULL FROM Users WHERE @userID IN (SELECT userID FROM Users WHERE deleted = 0)) 
 		BEGIN 
-			SELECT [errorMessage] = 'This user does not exist'
+			SELECT [error] = 'This user does not exist'
 		END 
 	ELSE
 		BEGIN
 			INSERT INTO Workouts(userID, templateID, [description], [date])
 			VALUES (@userID, @templateID, @description, @date) 
 
-			SELECT [workoutID] = @@IDENTITY, [message] = 'Workout successfully created'
+			SELECT [workoutID] = @@IDENTITY, * FROM Workouts
 		END
 
 
@@ -122,7 +138,7 @@ AS
 	
 	IF NOT EXISTS(SELECT NULL FROM Workouts WHERE workoutID = @workoutID)
 		BEGIN 
-			SELECT [errorMessage] = 'This workout does not exist'
+			SELECT [error] = 'This workout does not exist'
 		END 
 	ELSE 
 		BEGIN
@@ -133,7 +149,7 @@ AS
 				[date] = @date
 			WHERE workoutID = @workoutID
 
-			SELECT * , [message] = 'Workout was successfully updated' FROM Workouts WHERE workoutID = @workoutID
+			SELECT * FROM Workouts WHERE workoutID = @workoutID
 		END
 
 
@@ -145,18 +161,18 @@ AS
 	
 	IF NOT EXISTS(SELECT NULL FROM Workouts WHERE workoutID = @workoutID)
 		BEGIN 
-			SELECT [errorMessage] = 'This workout does not exist'
+			SELECT [error] = 'This workout does not exist'
 		END 
 	ELSE IF NOT EXISTS(SELECT NULL FROM Activities WHERE workoutID = @workoutID)
 		BEGIN
-			SELECT [errorMessage] = 'There is an activity that has this workout'
+			SELECT [error] = 'There is an activity that has this workout'
 		END 
 	ELSE 
 		BEGIN
 			DELETE FROM Workouts
 			WHERE workoutID = @workoutID
 
-			SELECT [message] = 'Workout has been deleted'
+			SELECT * FROM Workouts WHERE workoutID = @workoutID
 		END
 
 
@@ -168,13 +184,13 @@ AS
 
 	IF EXISTS(SELECT NULL FROM PersonalTrainers WHERE userID LIKE @UserID)
 		BEGIN
-			RETURN RAISERROR('error',15,1)
+			SELECT [error] = 'This user already has a trainer'
 		END 
 	ELSE
 		BEGIN
 			INSERT INTO PersonalTrainers(userID)
 			VALUES(@UserID)
-			SELECT [trainerID] = @@IDENTITY
+			SELECT [trainerID] = @@IDENTITY, userID FROM Users WHERE userID = @userID
 		END
 
 
@@ -190,10 +206,12 @@ AS
 			DELETE PersonalTrainers
 			FROM PersonalTrainers
 			WHERE @TrainerID LIKE trainerID
+
+			SELECT trainerID, userID FROM PersonalTrainers WHERE userID = @userID
 		END 
 	ELSE
 		BEGIN
-			RETURN RAISERROR('error',15,1)
+			SELECT [error] = 'User does not exist'
 		END
 
 
@@ -207,13 +225,13 @@ AS
 
 	IF EXISTS(SELECT NULL FROM Templates WHERE	(creatorID LIKE @CreatorID) AND (name LIKE @Name) AND ([description] LIKE @Description)) 
 		BEGIN
-				RETURN RAISERROR('error',15,1)
+			SELECT [error] = 'This template already exists'
 		END
 	ELSE 
 		BEGIN
 			INSERT INTO Templates(creatorID, name, [description])
 			VALUES(@CreatorID, @Name, @Description)
-			SELECT [templateID] = @@IDENTITY
+			SELECT [templateID] = @@IDENTITY, creatorID, [name], [description] FROM Templates 
 		END
 
 
@@ -230,14 +248,15 @@ SET NOCOUNT ON
 IF EXISTS(SELECT NULL FROM Templates WHERE	(templateID LIKE @TemplateID))
 	BEGIN
 		UPDATE Templates
-		SET creatorID	= @CreatorID,
-			name		= @Name,
+		SET creatorID = @CreatorID,
+			[name] = @Name,
 			[description] = @Description
 		WHERE templateID LIKE @TemplateID
+		SELECT * FROM Templates WHERE templateID = @TemplateID
 	END 
 ELSE 
 	BEGIN
-		RETURN RAISERROR('error',15,1)
+		SELECT [error] = 'Template could not be found'
 	END
 
 
@@ -252,10 +271,12 @@ AS
 			DELETE Templates
 			FROM Templates
 			WHERE templateID = @TemplateID
+
+			SELECT * FROM Templates WHERE templateID = @TemplateID
 		END 
 	ELSE 
 		BEGIN
-			RETURN RAISERROR('error',15,1)
+			SELECT [error] = 'Could not delete template'
 		END
 
 
