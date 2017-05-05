96fbfd8c8129b46aa325623f0979dc8a144be896
minikdj <minikdj11@gmail.com>
2017-05-05 13:21:53 -0400

    changed necessary things

--- CreateWorkoutDatabase.sql
+++ CreateWorkoutDatabase.sql
@@ -10,61 +10,73 @@ GO
 
 USE WorkoutDatabase;
 
-CREATE TABLE Users (
-	userID     int              PRIMARY KEY    IDENTITY(0,1),
-	name       varchar(255)     NOT NULL,
-	email      varchar(255)     NOT NULL,
-	[password] varbinary(128) NOT NULL,
-	birthday   date,
-	height     float,
-	[weight]   float,
-	trainer    bit,
-	deleted	   bit				DEFAULT(0)	
+CREATE TABLE [dbo].[Users] (
+    [userID]   INT             IDENTITY (1, 1) NOT NULL,
+    [name]     VARCHAR (255)   NOT NULL,
+    [email]    VARCHAR (255)   NOT NULL,
+    [password] VARBINARY (128) NOT NULL,
+    [birthday] DATE            NULL,
+    [height]   FLOAT (53)      NULL,
+    [weight]   FLOAT (53)      NULL,
+    [trainer]  BIT             NULL,
+    [deleted]  BIT             DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([userID] ASC)
 );
 
 
-CREATE TABLE PersonalTrainers (
-	trainerID int PRIMARY KEY	IDENTITY(0,1),
-	userID    int REFERENCES Users (userID)
+CREATE TABLE [dbo].[PersonalTrainers] (
+    [trainerID] INT IDENTITY (1, 1) NOT NULL,
+    [userID]    INT NULL,
+    PRIMARY KEY CLUSTERED ([trainerID] ASC),
+    FOREIGN KEY ([userID]) REFERENCES [dbo].[Users] ([userID])
+);
+
+CREATE TABLE [dbo].[Templates] (
+    [templateID]  INT           IDENTITY (1, 1) NOT NULL,
+    [creatorID]   INT           NULL,
+    [name]        VARCHAR (255) NOT NULL,
+    [description] VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([templateID] ASC),
+    FOREIGN KEY ([creatorID]) REFERENCES [dbo].[Users] ([userID])
+);
+CREATE TABLE [dbo].[Workouts] (
+    [workoutID]   INT           IDENTITY (1, 1) NOT NULL,
+    [userID]      INT           NULL,
+    [templateID]  INT           NULL,
+    [description] VARCHAR (255) NULL,
+    [date]        DATE          NULL,
+    PRIMARY KEY CLUSTERED ([workoutID] ASC),
+    FOREIGN KEY ([userID]) REFERENCES [dbo].[Users] ([userID]),
+    FOREIGN KEY ([templateID]) REFERENCES [dbo].[Templates] ([templateID])
 );
 
 
-CREATE TABLE Templates (
-	templateID int    PRIMARY KEY IDENTITY(0,1),
-	creatorID int     REFERENCES Users (userID),
-	name varchar(255) NOT NULL,
-	[description] varchar(255)
+CREATE TABLE [dbo].[Types] (
+    [typeID]      INT           IDENTITY (1, 1) NOT NULL,
+    [name]        VARCHAR (255) NOT NULL,
+    [description] VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([typeID] ASC)
 );
 
 
-CREATE TABLE Workouts (
-	workoutID     int            PRIMARY KEY IDENTITY(0,1),
-	userID        int            REFERENCES Users (userID),
-	templateID    int            REFERENCES Templates (templateID),
-	[description] varchar(255),
-	[date]        date
+
+CREATE TABLE [dbo].[Activities] (
+    [activityID] INT        IDENTITY (1, 1) NOT NULL,
+    [workoutID]  INT        NULL,
+    [templateID] INT        NULL,
+    [typeID]     INT        NULL,
+    [reps]       INT        NULL,
+    [sets]       INT        NULL,
+    [weight]     FLOAT (53) NULL,
+    [rest]       INT        NULL,
+    [distance]   FLOAT (53) NULL,
+    [time]       INT        NULL,
+    PRIMARY KEY CLUSTERED ([activityID] ASC),
+    FOREIGN KEY ([workoutID]) REFERENCES [dbo].[Workouts] ([workoutID]),
+    FOREIGN KEY ([templateID]) REFERENCES [dbo].[Templates] ([templateID]),
+    FOREIGN KEY ([typeID]) REFERENCES [dbo].[Types] ([typeID])
 );
 
-
-CREATE TABLE [Types] (
-	typeID        int PRIMARY KEY IDENTITY(0,1),
-	name          varchar(255) NOT NULL,
-	[description] varchar(255)
-);
-
-
-CREATE TABLE Activities (
-	activityID int   PRIMARY KEY IDENTITY(0,1),
-	workoutID  int   REFERENCES workouts (workoutID),
-	templateID int   REFERENCES Templates (templateID),
-	typeID     int   REFERENCES [Types] (typeID),
-	reps       int,
-	[sets]     int,
-	[weight]   float,
-	rest       int,
-	distance   float,
-	[time]     int
-);
 
 
 
